50e5fcc41c0fabdbfe72d8ad1b8544e9811f3e96
Mangesh Hambarde <mhambarde1@sheffield.ac.uk>
2019-02-24 01:31:27 +0000

    Update sql create script

--- mysql_create.sql
+++ mysql_create.sql
@@ -4,78 +4,87 @@ CREATE DATABASE mdp;
 
 USE mdp;
 
-CREATE TABLE `User` (
-	`user_id` int NOT NULL AUTO_INCREMENT,
-	`user_type` ENUM('LTM', 'ACADEMIC', 'YEARTUTOR') NOT NULL,
-	`user_email` char NOT NULL,
-	`user_password` char NOT NULL,
-	PRIMARY KEY (`user_id`),
-	UNIQUE KEY `unique_columns`(`user_email`)
+-- List of users of webapp.
+CREATE TABLE User (
+	user_id int AUTO_INCREMENT,
+	user_type ENUM('LTM', 'ACADEMIC', 'YEARTUTOR'),
+	user_email char,
+	user_password char,
+	PRIMARY KEY (user_id),
+	UNIQUE KEY unique_columns(user_email)
 );
 
-CREATE TABLE `Student` (
-	`student_id` int NOT NULL AUTO_INCREMENT,
-	`student_name` char NOT NULL,
-	PRIMARY KEY (`student_id`)
+-- List of students.
+CREATE TABLE Student (
+	student_id int AUTO_INCREMENT,
+	student_name char,
+	PRIMARY KEY (student_id)
 );
 
-CREATE TABLE `Module` (
-	`module_id` int NOT NULL AUTO_INCREMENT,
-	`module_code` char NOT NULL,
-	`module_name` char NOT NULL,
-	`module_semester` ENUM('FIRST', 'SECOND', 'BOTH') NOT NULL,
-	PRIMARY KEY (`module_id`),
-	UNIQUE KEY `unique_columns`(`module_code`, `module_semester`)
+-- List of modules with their assigned academic.
+CREATE TABLE Module (
+	mod_id int AUTO_INCREMENT,
+	mod_code char,
+	mod_name char,
+	mod_semester ENUM('FIRST', 'SECOND', 'BOTH'),
+	mod_academic INT,
+	PRIMARY KEY (mod_id),
+	UNIQUE KEY unique_columns(mod_code, mod_semester)
 );
 
-CREATE TABLE `Assessment` (
-	`ass_id` int NOT NULL AUTO_INCREMENT,
-	`ass_module` int NOT NULL,
-	`ass_semester` ENUM('FIRST', 'SECOND', 'BOTH') NOT NULL,
-	`ass_academic` int NOT NULL,
-	`ass_format` int NOT NULL,
-	`ass_name` char NOT NULL,
-	`ass_marks` FLOAT NOT NULL,
-	`ass_release_date` DATETIME NOT NULL,
-	`ass_submit_date` DATETIME NOT NULL,
-	PRIMARY KEY (`ass_id`)
+-- List of assessments.
+CREATE TABLE Assessment (
+	ass_id int AUTO_INCREMENT,
+	ass_format int,
+	ass_name char,
+	ass_marks FLOAT,
+	ass_release_date DATETIME,
+	ass_submit_date DATETIME,
+	PRIMARY KEY (ass_id)
 );
 
-CREATE TABLE `Form` (
-	`form_id` int NOT NULL AUTO_INCREMENT,
-	`form_timestamp` TIMESTAMP NOT NULL,
-	PRIMARY KEY (`form_id`)
+-- List of form created by LTM for each academic year.
+CREATE TABLE Form (
+	form_id int AUTO_INCREMENT,
+  form_name char,
+  form_state ENUM('CREATED', 'WAITING', 'DONE'),
+  form_acad_year_start DATETIME,
+  form_acad_year_end DATETIME,
+  form_exam_period_start DATETIME,
+  form_exam_period_end DATETIME,
+	PRIMARY KEY (form_id)
 );
 
-CREATE TABLE `StudentModule` (
-	`sm_module` int NOT NULL,
-	`sm_student` int NOT NULL,
-	PRIMARY KEY (`sm_module`,`sm_student`)
+-- List of entries in each form, one for each module.
+CREATE TABLE FormEntry (
+	entry_id int AUTO_INCREMENT,
+  entry_module INT,
+  entry_parent INT,
+	entry_timestamp TIMESTAMP,
+  entry_is_filled BOOLEAN,
+	PRIMARY KEY (entry_id)
 );
 
-CREATE TABLE `AcademicModule` (
-	`am_module` int NOT NULL,
-	`am_academic` int NOT NULL,
-	PRIMARY KEY (`am_module`, `am_academic`)
+-- List of assessments for a module.
+CREATE TABLE ModuleAssessments (
+	ma_module int,
+	ma_assessment int
 );
 
-CREATE TABLE `FormAssessment` (
-	`fa_form` int NOT NULL,
-	`fa_assessment` int NOT NULL
+-- List of modules each student is registered for.
+CREATE TABLE StudentModules (
+	sm_module int,
+	sm_student int,
+	PRIMARY KEY (sm_module,sm_student)
 );
 
-ALTER TABLE `Assessment` ADD CONSTRAINT `Assessment_fk0` FOREIGN KEY (`ass_module`) REFERENCES `Module`(`module_id`);
+ALTER TABLE Module ADD CONSTRAINT Module_fk0 FOREIGN KEY (mod_academic) REFERENCES User(user_id);
 
-ALTER TABLE `Assessment` ADD CONSTRAINT `Assessment_fk1` FOREIGN KEY (`ass_academic`) REFERENCES `User`(`user_id`);
+ALTER TABLE FormEntry ADD CONSTRAINT FormEntry_fk0 FOREIGN KEY (entry_module) REFERENCES Module(mod_id);
+ALTER TABLE FormEntry ADD CONSTRAINT FormEntry_fk1 FOREIGN KEY (entry_parent) REFERENCES Form(form_id);
 
-ALTER TABLE `StudentModule` ADD CONSTRAINT `StudentModule_fk0` FOREIGN KEY (`sm_module`) REFERENCES `Module`(`module_id`);
+ALTER TABLE StudentModules ADD CONSTRAINT StudentModule_fk0 FOREIGN KEY (sm_module) REFERENCES Module(mod_id);
+ALTER TABLE StudentModules ADD CONSTRAINT StudentModule_fk1 FOREIGN KEY (sm_student) REFERENCES Student(student_id);
 
-ALTER TABLE `StudentModule` ADD CONSTRAINT `StudentModule_fk1` FOREIGN KEY (`sm_student`) REFERENCES `Student`(`student_id`);
-
-ALTER TABLE `AcademicModule` ADD CONSTRAINT `AcademicModule_fk0` FOREIGN KEY (`am_module`) REFERENCES `Module`(`module_id`);
-
-ALTER TABLE `AcademicModule` ADD CONSTRAINT `AcademicModule_fk1` FOREIGN KEY (`am_academic`) REFERENCES `User`(`user_id`);
-
-ALTER TABLE `FormAssessment` ADD CONSTRAINT `FormAssessment_fk0` FOREIGN KEY (`fa_form`) REFERENCES `Form`(`form_id`);
-
-ALTER TABLE `FormAssessment` ADD CONSTRAINT `FormAssessment_fk1` FOREIGN KEY (`fa_assessment`) REFERENCES `Assessment`(`ass_id`);
+ALTER TABLE ModuleAssessments ADD CONSTRAINT ModuleAssessments_fk0 FOREIGN KEY (ma_module) REFERENCES Module(mod_id);
+ALTER TABLE ModuleAssessments ADD CONSTRAINT ModuleAssessments_fk1 FOREIGN KEY (ma_assessment) REFERENCES Assessment(ass_id);
